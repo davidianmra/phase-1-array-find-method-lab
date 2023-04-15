a5489ac1712ab74842fb36e330a13d9617a19f00
David Rashid <davidmra@umich.edu>
2023-04-15 00:28:21 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+
+function superbowlWin(records) {
+    const winningYear = records.find( record => record.result === "W")
+    if(!!winningYear){
+       
+        return winningYear.year
+
+      }
+    else{
+    return undefined
+    }     
+}
+
+
